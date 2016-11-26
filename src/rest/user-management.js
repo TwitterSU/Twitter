498f278ac1e42487049ff29cc5b55c9ed7b47d0a
vasilgn <vasilgn86@gmail.com>
2016-11-26 08:34:27 +0200

    crud

--- src/rest/user-management.js
+++ src/rest/user-management.js
@@ -1,5 +1,5 @@
 import {appID, appSecret, serviceBaseUrl} from './api.js';
-import $ from '../../node_modules/jquery/dist/jquery.min'
+import $ from '../../node_modules/jquery/dist/jquery.min';
 function errorHandler(response) {
   let errorMessage = JSON.stringify(response);
   if (response.readyState === 0) {
@@ -19,6 +19,26 @@ function successHandler(response) {
 export let BaseUserManagement = {
   login: (e) => {
     e.preventDefault();
+    // let body = JSON.stringify({
+    //   username: e.target[0].value,
+    //   password: e.target[1].value
+    // });
+    // fetch(serviceBaseUrl + "user/" + appID + '/login', {
+    //   method: 'POST',
+    //   headers: {
+    //     "Authorization": "Basic " + btoa(appID + ':' + appSecret),
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: body
+    // }).then((res) => {
+    //      res.json().then((data)=>{
+    //        sessionStorage.setItem('authToken', data._kmd.authtoken);
+    //      })
+    //
+    // })
+    // .catch((err) => {
+    //   console.log(err.json())
+    // });
     $.ajax({
       method: "POST",
       url: serviceBaseUrl + "user/" + appID + '/login',
@@ -34,8 +54,6 @@ export let BaseUserManagement = {
   },
   register: (e) => {
     e.preventDefault();
-
-    console.log(e);
     $.ajax({
       method: "POST",
       url: serviceBaseUrl + "user/" + appID + '/',
@@ -58,6 +76,21 @@ export let BaseUserManagement = {
       success: successHandler,
       error: errorHandler
     });
+  },
+  checkForExistingUser: (checkUserName) => {
+    $.ajax({
+      method: "POST",
+      url: serviceBaseUrl + "rpc/" + appID + '/check-username-exists',
+      headers: {"Authorization": "Basic " + btoa(appID + ':' + appSecret)},
+      data: {
+        "username": checkUserName
+      },
+      success: successHandler,
+      error: errorHandler
+    });
+    /*{
+     "usernameExists": "false" or "true"
+     }*/
   }
 };
 
